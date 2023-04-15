99a60ce60d13ff965fe227a7174fac60c3836945
Miguel Chavez <toonchavez8@gmail.com>
2023-04-15 13:39:21 -0600

    create 6 functions to ask user to add services
    created 6 functions to ask user to add services and to add them to the array
    
    the functions are:
        - displayServices
          that will display the services to the user
    
        - selectService (service)
          that will ask the user if the user wants to add the service to the array if not it will thank the user for using the page
    
        - orderSummery
            that will display the order summery to the user
    
        - updateOrderPrompt (service)
            that will ask the user if the user wants to update the order and if they do it will call the updateOrder function
    
        - updateOrder (service)
            that will ask the user wich service they want to update and will call the splcie the order from their service than it will display the order summery again

--- js/quiz.js
+++ js/quiz.js
@@ -19,7 +19,8 @@ const SERVICE = [
 	new Service("Animation", "Creative animation services for businesses.", 2500),
 ];
 
-let SELECTED_SERVICE = [];
+// This array will hold the services selected by the user
+const SELECTED_SERVICE = [];
 
 // This function displays the services available to the user
 function displayServices() {
@@ -52,10 +53,25 @@ function selectService() {
 				serviceIndex >= 0 &&
 				serviceIndex < SERVICE.length
 			) {
-				// Add the service to the order
-				SELECTED_SERVICE.push(SERVICE[serviceIndex]);
+				// Check if the service is already in the order
+				const selectedServiceNames = SELECTED_SERVICE.map(
+					(service) => service.name
+				);
+				if (selectedServiceNames.includes(SERVICE[serviceIndex].name)) {
+					alert(
+						`You already have '${SERVICE[serviceIndex].name}' added to your order. Please select a different service.`
+					);
+				} else {
+					// Add the service to the order
+					SELECTED_SERVICE.push(SERVICE[serviceIndex]);
+				}
 			} else {
-				alert("Invalid selection");
+				const availableServices = SERVICE.map(
+					(service, index) => `${index + 1}. ${service.name} \n`
+				).join("");
+				alert(
+					`That service doesnt exist. Please try again. Here are the services available \n\n ${availableServices} \n Try again with the number of the service you want to add to your order.`
+				);
 			}
 		}
 	} while (
@@ -66,7 +82,14 @@ function selectService() {
 	);
 
 	// Display the order summary
-	let orderPrompt = "Your order:\n";
+	orderSummery();
+}
+
+function orderSummery() {
+	// Update the order summary
+	let orderPrompt = "Your updated order:\n";
+
+	console.log("OrderSummery Func >", SELECTED_SERVICE);
 
 	const mapedServices = SELECTED_SERVICE.map(
 		(service) => `${service.name} - $${service.price} \n`
@@ -106,133 +129,102 @@ function selectService() {
 			}`;
 
 			break;
-		default:
-			orderPrompt += "You have not selected any services.";
+		default: // if the user didnt select any services we display a thank you message and exit the program
+			return thankYouAlert("thank you for visiting!");
 	}
 
 	alert(orderPrompt);
+
+	// then we prompt the user to update their order
+	updateOrderPrompt();
 }
 
-function updateOrder() {
+function updateOrderPrompt() {
 	let response = "";
-	// show the user the current order
 
 	// maped services with its id and price
 	const mapedServices = SELECTED_SERVICE.map(
 		(service, index) => `${index + 1}. ${service.name} - $${service.price} \n`
 	).join("");
 
+	// prompt the user if they want to update the order and convert it to lower case
 	response = prompt(
-		`this is your current order \n ${mapedServices} \n did you want to update your order?`
-	);
+		`These are your current services \n ${mapedServices}\n Would you like to update your order? (yes/no)`
+	).toLowerCase();
+
 	// check if the user wants to update the order
 
-	if (
-		response === "no" ||
-		response === "No" ||
-		response === "NO" ||
-		response === "nO" ||
-		response === "n" ||
-		response === "N"
-	) {
-		alert("Thank you for your order");
-	} else if (
-		response === "yes" ||
-		response === "Yes" ||
-		response === "YES" ||
-		response === "yEs" ||
-		response === "y" ||
-		response === "Y"
-	) {
-		// prompt the user to select the service they want to remove
-		response = prompt(
-			`These are your current services \n ${mapedServices}\n Enter the name of the service you would like to remove (or type 'done' to finish):`
-		);
-		// check if the user entered "done"
-		if (response !== null && response !== "" && response !== "done") {
-			// Get the service index from the user input
-			const serviceIndex = parseInt(response) - 1;
-			// Check if the service index is valid
-			if (
-				!isNaN(serviceIndex) &&
-				serviceIndex >= 0 &&
-				serviceIndex < SERVICE.length
-			) {
-				//ask the user to confirm they want to remove the service
-				response = prompt(
-					`Are you sure you want to remove ${SELECTED_SERVICE[serviceIndex].name} from your order?`
+	if (response === "no" || response === "n") {
+		return thankYouAlert();
+	} else if (response === "yes" || response === "y") {
+		updateOrder();
+	} else {
+		alert("Please only use yes or no to answer the question.");
+		updateOrderPrompt();
+	}
+}
+
+function updateOrder() {
+	let response = "";
+
+	// maped services with its id and price
+	const mapedServices_UOFunc = SELECTED_SERVICE.map(
+		(service, index) => `${index + 1}. ${service.name} - $${service.price} \n`
+	).join("");
+
+	console.log("UpdateOrder Func >", SELECTED_SERVICE);
+	// prompt the user to select the service they want to remove
+	response = prompt(
+		`These are your current services \n ${mapedServices_UOFunc}\n Enter the name of the service you would like to remove (or type 'done' to finish):`
+	);
+	// check if the user entered "done"
+	if (response !== null && response !== "" && response !== "done") {
+		// Get the service index from the user input
+		const serviceIndex = parseInt(response) - 1;
+		// Check if the service index is valid and remove the service from the order
+		if (
+			!isNaN(serviceIndex) &&
+			serviceIndex >= 0 &&
+			serviceIndex < SELECTED_SERVICE.length
+		) {
+			//ask the user to confirm they want to remove the service
+			response = prompt(
+				`Are you sure you want to remove ${SELECTED_SERVICE[serviceIndex].name} from your order?`
+			).toLowerCase();
+			// check if the user wants to remove the service
+			if (response === "no" || response === "n") {
+				updateOrder();
+			} else if (response === "yes" || response === "y") {
+				// propmt the user to select the service they want to add
+				alert(
+					`You have removed ${SELECTED_SERVICE[serviceIndex].name} from your order.`
 				);
-				// check if the user wants to remove the service
-				if (response === "no") {
-					alert("Thank you for your order");
-				} else if (
-					response === "yes" ||
-					response === "Yes" ||
-					response === "YES" ||
-					response === "yEs" ||
-					response === "y" ||
-					response === "Y"
-				) {
-					// remove the service from the order
-					SELECTED_SERVICE.splice(serviceIndex, 1);
-					// propmt the user to select the service they want to add
-				}
+				// remove the service from the order
+				SELECTED_SERVICE.splice(serviceIndex, 1);
+
+				console.log("UpdateOrder Func inner if function >", SELECTED_SERVICE);
+				orderSummery();
 			} else {
-				alert("Invalid selection");
+				alert("Please only use yes or no to answer the question.");
+				updateOrder();
 			}
-
-			// Update the order summary
-			let orderPrompt = "Your updated order:\n";
-
-			const mapedServices = SELECTED_SERVICE.map(
-				(service) => `${service.name} - $${service.price} \n`
-			).join("");
-
-			const total = SELECTED_SERVICE.reduce(
-				(total, service) => total + service.price,
-				0
+		} else {
+			alert(
+				`I'm sorry that service doesnt exist in your order. heres your current order \n ${mapedServices_UOFunc} \n Try again with the number of the service you want to remove from your order.`
 			);
 
-			// check the number of services selected and display the appropriate message with the total price
-			switch (SELECTED_SERVICE.length) {
-				case 1:
-					orderPrompt += `You have selected ${SELECTED_SERVICE.length} service. \n \n ${mapedServices} \n your total is $${total}`;
-					break;
-				case 2:
-					orderPrompt += `You have selected ${
-						SELECTED_SERVICE.length
-					} services. \n \n ${mapedServices} for a total worth of $${total} \n\n You will receive a 10% discount.\n your total is $${
-						total - total * 0.1
-					}`;
-
-					break;
-				case 3:
-					orderPrompt += `You have selected ${
-						SELECTED_SERVICE.length
-					} services. \n \n ${mapedServices}  for a total worth of $${total}\n\n You will receive a 20% discount.\n your total is $${
-						total - total * 0.2
-					}`;
-
-					break;
-				case 4:
-					orderPrompt += `You have selected ${
-						SELECTED_SERVICE.length
-					} services. \n \n ${mapedServices} \n for a total worth of $${total}\n\n You will receive a 30% discount.\n your total is $${
-						total - total * 0.3
-					}`;
-
-					break;
-				default:
-					orderPrompt += "You have not selected any services.";
-			}
-
-			alert(orderPrompt);
-		} else if (response === "done") {
-			alert("Thank you for your order");
-		} else {
-			alert("Invalid input");
+			updateOrder();
 		}
+	} else if (response === "done") {
+		// display the order summary
+		orderSummery();
 	}
+}
+
+// fucntion to thank the user for their order
+function thankYouAlert(message) {
+	const DEFAULT_MESSAGE = "Thank you for your order";
+	alert(`${message ? message : DEFAULT_MESSAGE} Have a great day!`);
 }
 
 // first we display the services available to the user
@@ -240,6 +232,3 @@ displayServices();
 
 // then we prompt the user to select the services they want to add to their order
 selectService();
-
-// then we prompt the user to update their order
-updateOrder(SELECTED_SERVICE);
