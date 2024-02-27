2ca16ccfcc94bb178251d41e7b2c18f7f46c165a
Miguel Chavez <toonchavez8@gmail.com>
2024-02-27 10:42:56 -0600

    feat: update layout with construction banner | add navbar

---
+++ nextjs-portfolio/app/components/NavBar/NavBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const NavBar = () => {
+	return (
+		<div className="absolute btn btn-ghost max-w-fit  md:text-xl inset-8 z-30 text-white capitalize font-semibold ">
+			&lt;toonchavez /&gt;
+		</div>
+	);
+};
