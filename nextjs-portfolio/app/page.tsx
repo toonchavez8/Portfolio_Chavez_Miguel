f858c1766d682f29b055d1ab92635e61b88d65f9
Miguel Chavez <toonchavez8@gmail.com>
2024-03-10 22:48:19 -0600

    Chore: Install Turbo | Feat: Set up Prietter

--- nextjs-portfolio/app/page.tsx
+++ nextjs-portfolio/app/page.tsx
@@ -1,8 +1,8 @@
 import Hero from "./components/hero";
 export default function Home() {
-	return (
-		<main className=" overflow-hidden  w-full max-w-[67.5rem] mx-auto relative flex flex-col items-center">
-			<Hero title="Miguel Chavez" sub="Full Stack Developer" />
-		</main>
-	);
+  return (
+    <main className="relative mx-auto flex w-full max-w-[67.5rem] flex-col  items-center overflow-hidden p-4 pt-2 ">
+      <Hero title="Miguel Chavez" sub="Full Stack Developer" />
+    </main>
+  );
 }
