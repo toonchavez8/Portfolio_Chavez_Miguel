1bcc7782f6506e2f7a6a8fe24095cecaecb0ffc7
Miguel Chavez <toonchavez8@gmail.com>
2024-03-13 00:54:16 -0600

    Chore: Rename components to UI

--- nextjs-portfolio/app/page.tsx
+++ nextjs-portfolio/app/page.tsx
@@ -1,11 +1,10 @@
-import Hero from './components/hero';
-import Projectsection from '@/components/projectsection';
+import Hero from '@/ui/hero';
+import Projectsection from '@/ui/projectsection';
 export default function Home() {
     return (
         <main className="relative mx-auto flex w-full max-w-[67.5rem] flex-col items-center gap-2 overflow-hidden p-4 md:gap-6 ">
             <Hero title="Miguel Chavez" sub="Full Stack Dev, Motion Designer and a bit more" />
             <Projectsection title="Miguel Chavez" />
-            <p />
         </main>
     );
 }
