255d973db927f45e45c8f0967e650182eaae5219
Miguel Chavez <toonchavez8@gmail.com>
2024-03-26 22:58:56 -0600

    feat: Add project list to stack page

--- nextjs-portfolio/app/projects/layout.tsx
+++ nextjs-portfolio/app/projects/layout.tsx
@@ -3,14 +3,14 @@ import Projectstacks from '@/app/ui/projectstack';
 
 const layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <main className=" mx-auto flex h-full w-11/12 flex-col md:w-7/12 md:flex-row md:overflow-hidden">
+        <section className="  mx-auto flex h-full w-11/12 flex-col md:w-7/12 md:flex-row md:overflow-hidden">
             <aside
-                className=" w-full flex-none md:flex md:w-1/4 md:flex-col"
+                className="  w-full flex-none md:flex md:w-1/6 md:max-w-full md:flex-col"
                 aria-label="project stacks">
                 <Projectstacks />
             </aside>
             <div className=" flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
-        </main>
+        </section>
     );
 };
 
