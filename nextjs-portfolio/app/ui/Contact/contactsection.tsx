27a200dad283ae0bb49366ad706059dcc4f614f4
Miguel Chavez <toonchavez8@gmail.com>
2024-04-01 14:08:11 -0600

    style(app): add twtich and remove group hover anim

--- nextjs-portfolio/app/ui/Contact/contactsection.tsx
+++ nextjs-portfolio/app/ui/Contact/contactsection.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Sectiontitles from '@/app/ui/atomic/sectiontitles';
 import { Separator } from '@/app/ui/atomic/separator';
 import { RiInstagramFill } from 'react-icons/ri';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaTwitch } from 'react-icons/fa';
 import { MdMailOutline } from 'react-icons/md';
 import Link from 'next/link';
 
@@ -24,17 +24,23 @@ const socialLinks = [
         url: 'https://www.linkedin.com/in/toonchavez8/',
         icon: <FaLinkedin />,
         text: 'toonchavez'
+    },
+    {
+        name: 'twitch',
+        url: 'https://www.twitch.tv/toonchavez_dev',
+        icon: <FaTwitch />,
+        text: 'toonchavez_dev'
     }
 ];
 
 const Contactsection = () => {
     return (
-        <section className=" group  mb-10 flex w-full flex-col justify-center gap-4  px-2 md:gap-6 md:px-4">
+        <section className="     mb-10 flex w-full flex-col  justify-center gap-4 px-2 md:gap-6 md:px-4">
             <Sectiontitles title="Contact" />
             <p className="normal-case">
                 Feel free to contact me on any of the social media links below or by email.
             </p>
-            <Separator className="animation-pulse w-0 opacity-50 transition-all ease-in-out group-hover:w-full" />
+            <Separator className=" w-full animate-pulse opacity-50 transition-all  ease-in-out" />
             <div className="flex  flex-wrap  items-center justify-center gap-4 text-sm md:gap-6">
                 {socialLinks.map((link) => (
                     <Link
