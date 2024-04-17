10a9ad2b1030d6c6bf9b83f6d26df8eff35bc9e7
Miguel Chavez <toonchavez8@gmail.com>
2024-04-17 13:35:02 -0600

    build(server): Update next.js to stable remove canary

--- nextjs-portfolio/next.config.mjs
+++ nextjs-portfolio/next.config.mjs
@@ -7,9 +7,6 @@ const nextConfig = {
     // Configure `pageExtensions` to include MDX files
     pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
     // Optionally, add any other Next.js config below
-    experimental: {
-        ppr: true
-    },
     images: {
         remotePatterns: [
             {
