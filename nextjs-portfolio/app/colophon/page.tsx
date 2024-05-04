5fb1a302585566f62625f82c9728cd72cd3386ae
Miguel Chavez <toonchavez8@gmail.com>
2024-05-04 07:56:05 -0600

    style(app): create array for tools and rename section

--- nextjs-portfolio/app/colophon/page.tsx
+++ nextjs-portfolio/app/colophon/page.tsx
@@ -1,5 +1,50 @@
 import React from 'react';
 import Sectiontitles from '@/app/ui/atomic/sectiontitles';
+import { url } from 'inspector';
+import Link from 'next/link';
+
+const toolsILike = [
+    {
+        name: 'VS Code',
+        url: 'https://code.visualstudio.com/',
+        description: 'My editor of choice'
+    },
+    {
+        name: 'Powertoys',
+        url: 'https://github.com/microsoft/PowerToys',
+        description: 'A set of utilities for Windows'
+    },
+    {
+        name: 'Notion',
+        url: 'https://www.notion.so/',
+        description: 'A note taking app '
+    },
+    {
+        name: 'Obsidian',
+        url: 'https://obsidian.md/',
+        description: 'A local note taking app with markdown support'
+    },
+    {
+        name: 'Arc Browser',
+        url: 'https://thebrowser.company/',
+        description: 'A browser with instresting tab functionalities and a lot of cool features'
+    },
+    {
+        name: 'Daily Dev',
+        url: 'https://daily.dev/',
+        description: 'A daily newsletter and information app about  development'
+    },
+    {
+        name: 'Bmrks.com',
+        url: 'https://bmrks.com/',
+        description: 'A minimalist bookmark site that I use to keep track of my favorite links'
+    },
+    {
+        name: 'Hero Icons',
+        url: 'https://heroicons.com/',
+        description: 'A set of icons that I use for my projects'
+    }
+];
 
 const Colophon = () => {
     return (
@@ -23,17 +68,16 @@ const Colophon = () => {
             <section className="flex flex-col gap-2 ">
                 <Sectiontitles title={'Stack'} />
                 <p>My personal list of tools that I use:</p>
-                <div>
-                    <ul className="list-inside list-disc font-mono">
-                        <li>VSCode</li>
-                        <li>Powertoys</li>
-                        <li>Notion</li>
-                        <li>Obsidien</li>
-                        <li>Arc Browser</li>
-                        <li>Daily Dev</li>
-                        <li>Bmrks.com</li>
-                        <li>Hero Icons</li>
-                    </ul>
+                <div className="flex flex-col gap-2">
+                    {toolsILike.map((tool) => (
+                        <Link
+                            href={tool.url}
+                            key={tool.name}
+                            className="  badge badge-outline  hover:text-primary">
+                            {tool.name}{' '}
+                            <span className="ml-2 text-xs text-gray-500 ">{tool.description}</span>
+                        </Link>
+                    ))}
                 </div>
             </section>
         </main>
