db20e2d1e76747f28cb86872e4e1723031659c6d
Miguel Chavez <toonchavez8@gmail.com>
2024-05-13 12:58:57 -0600

    feat(ui): Update styles for BentoGridItem and NowSection Skeleton components.

--- nextjs-portfolio/app/ui/Now/nowSection.tsx
+++ nextjs-portfolio/app/ui/Now/nowSection.tsx
@@ -17,22 +17,47 @@ export function NowSection() {
         </BentoGrid>
     );
 }
-const Skeleton = () => (
-    <div className=" \ flex h-full min-h-[6rem] w-full flex-1  rounded-xl border border-primary  bg-neutral-100 bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)] dark:border-white/[0.2] dark:bg-black dark:bg-dot-white/[0.2] "></div>
+const Skeleton = ({
+    children
+}: Readonly<{
+    children?: React.ReactNode;
+}>) => (
+    <div className=" \ relative flex h-full min-h-[6rem] w-full  flex-1 flex-col items-start justify-center  rounded-xl border border-primary bg-neutral-100 px-6 bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)] dark:border-white/[0.2] dark:bg-black dark:bg-dot-white/[0.2]">
+        {children}
+    </div>
 );
 const items = [
     {
         title: 'What im learning now',
         description:
             "I'm learning Python, data structures, and algorithms through Stanford's 'Learn to Code' mini-course and FreeCodeCamp",
-        header: <Skeleton />,
+        header: (
+            <Skeleton>
+                <figure className=" flex aspect-square w-2/3 flex-col items-start justify-around  py-1 transition-all duration-500 group-hover/bento:animate-pulse">
+                    <div className="h-4 w-4   rounded-full bg-primary transition-all duration-150 group-hover/bento:w-full" />
+                    <div className="h-4 w-4   rounded-full bg-accent transition-all duration-150 group-hover/bento:w-2/3" />
+                    <div className="h-4 w-4   rounded-full bg-primary transition-all duration-150 group-hover/bento:w-4/5" />
+                    <div className="h-4 w-4   rounded-full bg-secondary transition-all duration-150 group-hover/bento:w-1/3" />
+                </figure>
+            </Skeleton>
+        ),
         className: 'md:col-span-2',
         icon: <IconNotebook className="h-4 w-4 text-primary " />
     },
     {
         title: 'What im reading?',
         description: 'Dune.',
-        header: <Skeleton />,
+        header: (
+            <Skeleton>
+                <figure className="  debug flex h-full w-full max-w-fit flex-col  items-center justify-center py-1 transition-all  duration-500 group-hover/bento:-rotate-6 group-hover/bento:scale-110">
+                    <img
+                        src="https://assets.literal.club/4/ckikds6gf01560zhfha1gtsvm.jpg?size=600"
+                        alt="dune book cover"
+                        className="rounded-sm"
+                    />
+                </figure>
+            </Skeleton>
+        ),
         className: 'md:col-span-1',
         icon: <IconFileBroken className="h-4 w-4 text-primary" />
     },
