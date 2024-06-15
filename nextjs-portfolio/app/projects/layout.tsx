37bac6e549107e4973a05f087287dccee83b97fb
Miguel Chavez <toonchavez8@gmail.com>
2024-06-15 08:27:41 -0600

    refactor(ui): Add transition effect to section titles for better user experience.

--- nextjs-portfolio/app/projects/layout.tsx
+++ nextjs-portfolio/app/projects/layout.tsx
@@ -3,9 +3,9 @@ import Projectstacks from '@/app/ui/projectstack';
 
 const layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <section className="  mx-auto flex h-full w-11/12 flex-col md:w-7/12 md:flex-row md:overflow-hidden">
+        <section className="   mx-auto flex h-full w-11/12 flex-col md:w-7/12 md:flex-row md:overflow-hidden">
             <aside
-                className="  w-full flex-none md:flex md:w-1/6 md:max-w-full md:flex-col"
+                className=" group/section w-full flex-none md:flex md:w-1/6 md:max-w-full md:flex-col"
                 aria-label="project stacks">
                 <Projectstacks />
             </aside>
--- nextjs-portfolio/app/ui/projectstack.tsx
+++ nextjs-portfolio/app/ui/projectstack.tsx
@@ -7,7 +7,7 @@ const Projectstacks = async () => {
 
     return (
         <div className="top-10 flex flex-wrap items-start justify-center gap-4 px-2 md:fixed md:mt-20 md:flex-col md:gap-6 md:px-4">
-            <h2 className="font-mono text-lg  opacity-60 " aria-label="Number of Stacks">
+            <h2 className="font-mono text-lg  opacity-60 transition-colors duration-150 group-hover/section:text-viridian-600 dark:group-hover/section:text-viridian-500">
                 <span className="animate-pulse">&#47;&#47; </span>
                 {filteredStacks.length} Stacks
             </h2>
