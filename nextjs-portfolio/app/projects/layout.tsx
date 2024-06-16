06927c4dba44b40a133f526238b1f8aeea2b4bd5
Miguel Chavez <toonchavez8@gmail.com>
2024-06-16 13:43:15 -0600

    style: update layout class in React component to fix styling issue on larger screens

--- nextjs-portfolio/app/projects/layout.tsx
+++ nextjs-portfolio/app/projects/layout.tsx
@@ -3,7 +3,7 @@ import Projectstacks from '@/app/ui/projectstack';
 
 const layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <section className="   mx-auto flex h-full w-11/12 flex-col md:w-7/12 md:flex-row md:overflow-hidden">
+        <section className=" mx-auto flex h-full w-11/12 flex-col md:w-11/12 md:flex-row md:overflow-hidden lg:w-10/12">
             <aside
                 className=" group/section w-full flex-none md:flex md:w-1/6 md:max-w-full md:flex-col"
                 aria-label="project stacks">
