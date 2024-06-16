bc276b2845f93012c2039143ac7ffe32aa3c952f
Miguel Chavez <toonchavez8@gmail.com>
2024-06-17 01:31:00 +0900

    style: update padding and alignment  component for better spacing on accordian education and pjct sction

--- nextjs-portfolio/app/ui/projectSection/projectSection.tsx
+++ nextjs-portfolio/app/ui/projectSection/projectSection.tsx
@@ -12,7 +12,7 @@ const Projectsection = (props: ProjectProps) => {
     const projects = getStarredProjectsData();
 
     return (
-        <section className="group/section flex w-full flex-col justify-center gap-4 px-2 md:gap-6 md:px-4">
+        <section className="group/section flex w-full flex-col justify-center gap-4 py-2 md:gap-6 md:px-4">
             <Sectiontitles title={props.title} />
             <div className="grid w-full grid-cols-1 gap-4 ">
                 {projects.map((project) => (
