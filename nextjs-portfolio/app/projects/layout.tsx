f12187436fc6a95bacab5bab8e6c77e08f6a5ac5
Miguel Chavez <toonchavez8@gmail.com>
2024-06-17 18:52:11 -0600

    style: Adjust styling in project stack container, article figure, and layout.

--- nextjs-portfolio/app/projects/layout.tsx
+++ nextjs-portfolio/app/projects/layout.tsx
@@ -3,13 +3,13 @@ import Projectstacks from '@/app/ui/projectstack';
 
 const layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <section className=" mx-auto flex h-full w-11/12 flex-col md:w-11/12 md:flex-row md:overflow-hidden lg:w-10/12">
+        <section className="   mx-auto flex h-full w-11/12 flex-col md:w-10/12 md:flex-row md:overflow-hidden lg:w-7/12">
             <aside
-                className=" group/section w-full flex-none md:flex md:w-1/6 md:max-w-full md:flex-col"
+                className=" group/section mt-4 w-full flex-none md:flex md:w-1/6 md:max-w-full md:flex-col "
                 aria-label="project stacks">
                 <Projectstacks />
             </aside>
-            <div className=" flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+            <div className="  flex-grow sm:p-6 md:p-12">{children}</div>
         </section>
     );
 };
